03f43379981aed3c5cff689d19fc6a6db81590d9
Abhishek Verma <nonuabi@gmail.com>
2025-10-10 18:27:55 +0530

    Add frozen string literal comment to group and group_member models; update schema version format

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class Group < ApplicationRecord
   belongs_to :owner, class_name: 'User', foreign_key: 'owner_id'
   has_many :group_members
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_10_08_135148) do
+ActiveRecord::Schema[8.0].define(version: 20_251_008_135_148) do
   # These are extensions that must be enabled in order to support this database
   enable_extension 'pg_catalog.plpgsql'
 
